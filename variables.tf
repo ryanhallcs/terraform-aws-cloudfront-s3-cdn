74f3d342a3c8c3263444bcb969bb1bacb3ff2e7f
Ryan Hall <ryan.hall@pager.com>
2020-02-01 15:05:58 -0500

    fix variables typo

--- variables.tf
+++ variables.tf
@@ -91,7 +91,6 @@ variable "alias_paths" {
   # http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/distribution-web-values-specify.html#DownloadDistValuesOriginPath
   type        = map(string)
   description = "An optional element that causes CloudFront to request your content from a directory in your Amazon S3 bucket or your custom origin. It must begin with a /. Do not add a / at the end of the path."
-  default     = "{}"
 }
 
 variable "origin_force_destroy" {
